01e802a682e20e36150038dac7f06a52830f3ae9
Yash shah <76220716+coder2402@users.noreply.github.com>
2022-01-15 16:01:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+
+const port = 3000
+const app = express()
+const fs = require('fs')
+
+app.set('view engine', 'ejs')
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+
+
+app.post("/", function(req, res){
+    const city = req.body.city
+    console.log(city)
+    //res.render("Advisory.ejs")
+})
+
+app.listen(port)
+
